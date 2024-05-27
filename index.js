b120235f4f3d1ec06ab4d9449634978b6ddd939e
kmr-ankitt <sahuankit.murhu@gmail.com>
2024-05-27 16:06:19 +0530

    deployment setting

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ const __dirname = path.dirname(__filename);
 // Middlewares
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.static("public"));
+// app.use(express.static("public"));
+app.use(express.static(__dirname + "/public/"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
