5b9c7c6465d09b455e380c23e3f71fed326d02cc
kmr-ankitt <sahuankit.murhu@gmail.com>
2024-05-27 12:41:47 +0530

    animations

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -10,3 +10,20 @@ gsap.to("#second-page h2", {
     pin: true,
   }
 });
+
+gsap.from("#navbar", {
+  y: "-10%",
+  opacity: 0,
+  duration: 2,
+  // delay: 1
+})
+
+gsap.from(".third-page-container .view-title h2 , .third-page-container h3 ",{
+  x: -500,
+  duration: 2,
+  opacity: 0,
+  scrollTrigger: {
+      trigger:"#third-page",
+      scroller: "body",
+  }
+})
