617ed467bc4e5fdc6e47d67e0198ee742ee386d6
kmr-ankitt <sahuankit.murhu@gmail.com>
2024-05-27 17:15:28 +0530

    Responsive bugs

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -4,8 +4,8 @@ gsap.to("#second-page h2", {
   x: "-80%", 
   scrollTrigger: {
     trigger: "#second-page", 
-    start: "top top", 
-    end: "bottom top", 
+    start: "top 50%", 
+    end: "bottom 50%", 
     scrub: 2,
     pin: true,
   }
@@ -16,14 +16,4 @@ gsap.from("#navbar , .about-con", {
   opacity: 0,
   duration: 2,
   // delay: 1
-})
-
-gsap.from(".third-page-container .view-title h2 , .third-page-container h3 ", {
-  x: -500,
-  duration: 2,
-  opacity: 0,
-  scrollTrigger: {
-      trigger:"#third-page",
-      scroller: "body",
-  }
 })
